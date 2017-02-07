c3295447d5f33bb512498814f8512b4b67c9a42d
Kevin Isom <kevin.isom@gmail.com>
2017-02-07 16:35:57 +1300

    make it work

--- src/calculator.js
+++ src/calculator.js
@@ -50,7 +50,7 @@ class Calculator extends Component {
                 return <InputButton
                             value={buttonVal}
                             highlight={this.state.selectedSymbol === buttonVal}
-                            onPress={()=>{}}
+                            onPress={this._onInputButtonPressed.bind(this, buttonVal)}
                             key={'butt-' + columnIdx} />;
             });
 
@@ -60,6 +60,62 @@ class Calculator extends Component {
         return views;
     }
 
+    _onInputButtonPressed(input) {
+        switch (typeof input) {
+            case 'number':
+                return this._handleNumberInput(input);
+            default:
+                return this._handleStringInput(input);
+        }
+    }
+
+    _handleNumberInput(num) {
+        let inputValue = (this.state.inputValue * 10) + num;
+
+        this.setState({
+            inputValue: inputValue
+        });
+    }
+
+    _handleStringInput(str) {
+        switch (str) {
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                this.setState({
+                    selectedSymbol: str,
+                    previousInputValue: this.state.inputValue,
+                    inputValue: 0
+                });
+                break;
+
+            case '=':
+                let symbol = this.state.selectedSymbol,
+                    inputValue = this.state.inputValue,
+                    previousInputValue = this.state.previousInputValue;
+
+                if (!symbol) {
+                    return;
+                }
+
+                this.setState({
+                    previousInputValue: 0,
+                    inputValue: eval(previousInputValue + symbol + inputValue),
+                    selectedSymbol: null
+                });
+                break;
+
+            case 'ce':
+                this.setState(this.initialState);
+                    break;
+
+            case 'c':
+                this.setState({inputValue: 0});
+                break;
+
+        }
+    }
 }
 
 AppRegistry.registerComponent('Calculator', () => Calculator);
